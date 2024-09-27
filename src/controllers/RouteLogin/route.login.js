dda1ac71f6a25d2468cf044b567bb05a874b3edb
analistamlc <122987725+analistamlc@users.noreply.github.com>
2024-09-27 14:53:24 -0500

    fixed: token (JWT) MLC Biblioteca

--- src/controllers/RouteLogin/route.login.js
+++ src/controllers/RouteLogin/route.login.js
@@ -1,11 +1,15 @@
 import CustomError from "../../errors/CustomErros.js";
 import { UserBannedError } from "../../errors/UserBannedError.js";
 import { validacionUsuario } from "../../querys/Login/login.js";
+import { generateJWT } from "../jwt/loginToken.js";
+
 export const controladorRutaLoginPost = async (req, res) => {
     const { email, password } = req.body;
     try
     {
         const userData = await validacionUsuario(email, password);
+        const token = await generateJWT(userData)
+        res.cookie('BibliotecaMLC', token, { httpOnly: true, secure: true, sameSite: 'strict' });
         res.status(200).json({ Data: userData, success: true, error: false });
     } 
     catch (error)

0b811230d4c7e053c7ef0c00b144fe74bed01c89
analistamlc <122987725+analistamlc@users.noreply.github.com>
2024-11-18 14:29:45 -0500

    add: Roue nueva solicitud

--- src/Schemas/Datos/dataSolicitud.js
+++ src/Schemas/Datos/dataSolicitud.js
@@ -1,31 +1,43 @@
-import { construirFechaUTC5, ObtenerFechaYHoraActual } from "../Fechas/fechasHoras.js";
+import {ObtenerFechaYHoraActual } from "../Fechas/fechasHoras.js";
 
 export const ordernarDatosDeEntrada = (data, user) => {
-    const rol = user.nombre_rol
-    const Data ={
-        id_Archivo: data.fileId,
-        CommentsOperador: data.motivo_solicitud.length > 500 ? data.motivo_solicitud.slice(0, 500) : data.motivo_solicitud
-    };
+    const rol = user.nombre_rol;
+    
+    let fecha_solicitud;
+    let OPE_Comentario = null;
+    let ADM_Comentario = null;
+    let ADM_Aproved = false;
+    const id_Archivo = data.fileId;
+    let OPE_UserID = null;
+    let ADM_UserID = null;
+    let Estado_Solicitud = null
 
-    let fecha_solicitud
-    let OPE_Comentario
-    //const { userName, textAreaValue, fileName, OPEUserName, OPEComment, fileId, approvedGER, folder, approvedADM } = req.body;
-
-    console.log(user)
-    console.log(rol)
-    console.log(data)
-
-    if(rol =='OPE')
-    {
-        fecha_solicitud = ObtenerFechaYHoraActual()
-        OPE_Comentario = data.motivo_solicitud.length > 500 ? data.motivo_solicitud.slice(0, 500) : data.motivo_solicitud
+    if (rol === 'OPE') {
+        fecha_solicitud = ObtenerFechaYHoraActual();
+        OPE_Comentario = data.motivo_solicitud.length > 500 ? data.motivo_solicitud.slice(0, 500).toUpperCase(): data.motivo_solicitud.toUpperCase();
+        OPE_UserID = user.id_usuario
+        Estado_Solicitud='SOLICITADA'
     }
 
-    if(rol =='ADM')
-        {
-            fecha_solicitud = ObtenerFechaYHoraActual()
-            OPE_Comentario = data.motivo_solicitud.length > 500 ? data.motivo_solicitud.slice(0, 500) : data.motivo_solicitud
-        }
+    if (rol === 'GER') {
+        fecha_solicitud = ObtenerFechaYHoraActual();
+        ADM_Comentario = data.motivo_solicitud.length > 500 ? data.motivo_solicitud.slice(0, 500).toUpperCase() : data.motivo_solicitud.toUpperCase();
+        OPE_Comentario = data.motivo_solicitud.length > 500 ? data.motivo_solicitud.slice(0, 500).toUpperCase() : data.motivo_solicitud.toUpperCase();
+        ADM_Aproved = true;
+        OPE_UserID = user.id_usuario
+        ADM_UserID = user.id_usuario
+        Estado_Solicitud='APROVED GERENTE'
+    }
 
-    return Data
+    return {
+        Fecha_Solicitud: fecha_solicitud,
+        OPE_Comentario: OPE_Comentario,
+        ADM_Comentario: ADM_Comentario,
+        ADM_Aproved: ADM_Aproved,
+        ID_Archivo: id_Archivo,
+        OPE_UserID: OPE_UserID,
+        ADM_UserID: ADM_UserID || null,
+        Estado_Solicitud:Estado_Solicitud
+    };
 };
+
--- src/controllers/Solicitudes/Ctr_Solicitudes.js
+++ src/controllers/Solicitudes/Ctr_Solicitudes.js
@@ -1,17 +1,18 @@
-import { listFilesInDrive, ObtenerArchivoDesdeDrive } from "../../Drive/driveService.js";
 import { verificar_Permiso_Para_Archivo } from "../../querys/Files/db_files.js";
 import { db_Insertar_Solicitud_Nueva } from "../../querys/solicitudes/db.solicitudes.js";
 import { ordernarDatosDeEntrada } from "../../Schemas/Datos/dataSolicitud.js";
 
 
 export const ctr_AgregarNuevaSolicitud = async (req, res) => {
+    const rol = req.user.nombre_rol
+
     const data = ordernarDatosDeEntrada(req.body, req.user)
     const ArchivosPermitidos = await verificar_Permiso_Para_Archivo(rol, data.id_Archivo)
-
+    
     if(ArchivosPermitidos)
     {
         
-        const insertarDatos = await db_Insertar_Solicitud_Nueva(data)
+      console.log(data)
     }
     res.json({ success: false });
 };
